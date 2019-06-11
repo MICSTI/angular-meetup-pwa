1bced67fc5e698e15b97499c2c80af8298c98f7f
Michael Stifter <stifter@parkside.at>
2019-06-11 23:45:35 +0200

    Add basic notification permission request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+
+declare var window: any;
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
-  title = 'angular-meetup-pwa';
+  title = "angular-meetup-pwa";
+
+  requestNotificationPermission = async () => {
+    const permission = await window.Notification.requestPermission();
+    // value of permission can be 'granted', 'default', 'denied'
+    // granted: user has accepted the request
+    // default: user has dismissed the notification permission popup by clicking on x
+    // denied: user has denied the request.
+    console.log("Notification permission", permission);
+
+    if (permission !== "granted") {
+      throw new Error("Permission not granted for Notification");
+    }
+  };
 }
