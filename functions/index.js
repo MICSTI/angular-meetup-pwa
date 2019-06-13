ddd8c94ead8d512c91479c4beae711a8e589ec8c
Michael Stifter <stifter@parkside.at>
2019-06-13 11:49:16 +0200

    Implement getSubscripts cloud function

--- functions/index.js
+++ functions/index.js
@@ -4,11 +4,30 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
+const fetchSubscriptions = async () => {
+  return await admin
+    .database()
+    .ref("/subscriptions")
+    .once("value");
+};
+
 exports.hello = functions.https.onRequest((req, res) => {
   res.send("Hello from the Angular Meetup PWA!");
 });
 
 exports.addSubscription = functions.https.onRequest(async (req, res) => {
+  if (req.method !== "POST") {
+    return res.status(400).json({
+      message: "No handler for request method " + req.method
+    });
+  }
+
+  if (req.get("content-type") !== "application/json") {
+    return res.status(400).json({
+      message: "Only Content-Type application/json is supported"
+    });
+  }
+
   const data = req.body.data;
 
   if (!data) {
@@ -22,14 +41,16 @@ exports.addSubscription = functions.https.onRequest(async (req, res) => {
     .ref("/subscriptions")
     .push(data);
 
-  res.status(201).json(snapshot.ref);
+  return res.status(201).send();
 });
 
-exports.getSubscriptions = functions.https.onRequest((req, res) => {
-  res.send("Implement me!");
+exports.getSubscriptions = functions.https.onRequest(async (req, res) => {
+  const subscriptions = await fetchSubscriptions();
+
+  return res.status(200).json(subscriptions);
 });
 
-exports.removeSubscription = functions.htttps.onRequest((req, res) => {
+exports.removeSubscription = functions.https.onRequest((req, res) => {
   res.send("Implement me!");
 });
 
