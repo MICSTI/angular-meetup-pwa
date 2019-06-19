80bc80f796e9ce2c76134943ad97c151a3904d74
Michael Stifter <stifter@parkside.at>
2019-06-19 10:43:11 +0200

    refactor: improve logic of sending push notifications

--- functions/index.js
+++ functions/index.js
@@ -1,21 +1,21 @@
-const functions = require("firebase-functions");
+const functions = require('firebase-functions');
 
-const express = require("express");
-const cors = require("cors");
-const bodyParser = require("body-parser");
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
 const app = express();
 
 // set up body parser
 app.use(bodyParser.json());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Automatically allow cross-origin requests
 app.use(cors({ origin: true }));
 
 // The Firebase Admin SDK to access the Firebase Realtime Database.
-const admin = require("firebase-admin");
+const admin = require('firebase-admin');
 admin.initializeApp();
 
 const getRandomSubscription = async () => {
@@ -34,46 +34,85 @@ const getAllSubscriptions = async () => {
 
   const snapshot = await admin
     .database()
-    .ref("/subscriptions")
+    .ref('/subscriptions')
     .orderByKey()
-    .once("value");
+    .once('value');
 
-  snapshot.forEach(childSnapshot => {
+  snapshot.forEach((childSnapshot) => {
     const value = childSnapshot.val();
 
     subscriptions.push({
       token: value.token,
-      name: value.name
+      name: value.name,
     });
   });
 
   return subscriptions;
 };
 
-const tokenExists = async token => {
-  let exists = false;
+const broadcastMessage = async (messageData, excludedTokens = []) => {
+  const enabled = await broadcastEnabled();
+
+  if (!enabled) {
+    return false;
+  }
+
+  const subscriptions = await getAllSubscriptions();
+
+  const filteredSubscriptions = subscriptions.filter(
+    (subscription) => {
+      return !excludedTokens.includes(subscription.token);
+    }
+  );
+
+  filteredSubscriptions.forEach(subscription => {
+    await sendSinglePushMessage(subscription.token, messageData);
+  });
+
+  return true;
+};
+
+const getSubscriptionToToken = async (token) => {
+  let subscription = null;
 
   const snapshot = await admin
     .database()
-    .ref("/subscriptions")
+    .ref('/subscriptions')
     .orderByKey()
-    .once("value");
+    .once('value');
 
-  snapshot.forEach(childSnapshot => {
+  snapshot.forEach((childSnapshot) => {
     const value = childSnapshot.val();
 
     if (value.token === token) {
-      exists = true;
+      subscription = value;
     }
   });
 
-  return exists;
+  return subscription;
+};
+
+const tokenExists = async (token) => {
+  const subscription = await getSubscriptionToToken(token);
+
+  return subscription !== null;
 };
 
 const sendSinglePushMessage = async (token, data) => {
   const message = {
-    data,
-    token
+    token,
+    notification: {
+      title: data.title,
+    },
+    webpush: {
+      headers: {
+        Urgency: 'high',
+      },
+      notification: {
+        body: data.body,
+        requireInteraction: 'true',
+      },
+    },
   };
 
   try {
@@ -83,14 +122,52 @@ const sendSinglePushMessage = async (token, data) => {
   }
 };
 
-app.get("/hello", (req, res) => {
-  res.send("Hello from the Angular Meetup PWA!");
+const broadcastEnabled = async () => {
+  const snapshot = await admin
+    .database()
+    .ref('/broadcast')
+    .orderByKey()
+    .once('value');
+
+  return snapshot.length > 0;
+};
+
+const activateBroadcast = async () => {
+  const alreadyEnabled = await broadcastEnabled();
+
+  if (alreadyEnabled) {
+    return true;
+  }
+
+  const data = {
+    active: true,
+  };
+
+  const snapshot = await admin
+    .database()
+    .ref('/broadcast')
+    .push(data);
+
+  return true;
+};
+
+const deactivateBroadcast = async () => {
+  const snapshot = await admin
+    .database()
+    .ref('/broadcast')
+    .remove();
+
+  return true;
+};
+
+app.get('/hello', (req, res) => {
+  res.send('Hello from the Angular Meetup PWA!');
 });
 
-app.post("/addSubscription", async (req, res) => {
-  if (req.get("content-type") !== "application/json") {
+app.post('/addSubscription', async (req, res) => {
+  if (req.get('content-type') !== 'application/json') {
     return res.status(400).json({
-      message: "Only Content-Type application/json is supported"
+      message: 'Only Content-Type application/json is supported',
     });
   }
 
@@ -98,7 +175,7 @@ app.post("/addSubscription", async (req, res) => {
 
   if (!data) {
     return res.status(400).json({
-      message: "Missing body data"
+      message: 'Missing body data',
     });
   }
 
@@ -110,56 +187,103 @@ app.post("/addSubscription", async (req, res) => {
 
   const snapshot = await admin
     .database()
-    .ref("/subscriptions")
+    .ref('/subscriptions')
     .push(data);
 
   return res.status(201).send();
 });
 
-app.get("/getSubscriptions", async (req, res) => {
+app.get('/getSubscriptions', async (req, res) => {
   const subscriptions = await getAllSubscriptions();
 
   return res.status(200).json(subscriptions);
 });
 
-app.get("/getRandom", async (req, res) => {
+app.get('/getRandom', async (req, res) => {
   const randomSubscription = await getRandomSubscription();
 
   return res.status(200).json(randomSubscription);
 });
 
-app.post("/removeSubscription", (req, res) => {
-  res.send("Implement me!");
+app.post('/removeSubscription', (req, res) => {
+  res.send('Implement me!');
 });
 
-app.delete("/clearAllSubscriptions", async (req, res) => {
+app.delete('/clearAllSubscriptions', async (req, res) => {
   const snapshot = await admin
     .database()
-    .ref("/subscriptions")
+    .ref('/subscriptions')
     .remove();
 
   return res.status(204).send();
 });
 
-app.post("/sendPushMessage", async (req, res) => {
-  if (req.get("content-type") !== "application/json") {
+app.post('/sendPushMessage', async (req, res) => {
+  if (req.get('content-type') !== 'application/json') {
     return res.status(400).json({
-      message: "Only Content-Type application/json is supported"
+      message: 'Only Content-Type application/json is supported',
     });
   }
 
-  const { token, name } = req.body.data;
+  const { token, title, body } = req.body.data;
+
+  const subscription = await getSubscriptionToToken(token);
+
+  if (!subscription) {
+    return res
+      .status(400)
+      .json({ message: 'No matching token found' });
+  }
 
   try {
     await sendSinglePushMessage(token, {
-      title: `Hi ${name}`,
-      message: "Hello!!!"
+      title,
+      body,
     });
 
-    return res.status(200).send();
+    return res.status(204).send();
   } catch (ex) {
     return res.status(400).json({
-      message: ex.message
+      message: ex.message,
+    });
+  }
+});
+
+app.post('/enableBroadcast', async (req, res) => {
+  const status = await activateBroadcast();
+
+  return res.status(200).json({
+    status,
+  });
+});
+
+app.post('/disableBroadcast', async (req, res) => {
+  const status = await deactivateBroadcast();
+
+  return res.status(200).json({
+    status,
+  });
+});
+
+app.post('/broadcastMessage', async (req, res) => {
+  if (req.get('content-type') !== 'application/json') {
+    return res.status(400).json({
+      message: 'Only Content-Type application/json is supported',
+    });
+  }
+
+  const { senderToken, title, body } = req.body.data;
+
+  try {
+    await broadcastMessage({
+      title,
+      body,
+    }, [senderToken]);
+
+    return res.status(204).send();
+  } catch (ex) {
+    return res.status(400).json({
+      message: ex.message,
     });
   }
 });
