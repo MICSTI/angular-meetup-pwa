66633dcaadd3131839ade34b598caaa492f69eda
Michael Stifter <stifter@parkside.at>
2019-06-21 08:22:57 +0200

    chore: add Parkside icons

--- src/app/sw/firebase-messaging-sw.js
+++ src/app/sw/firebase-messaging-sw.js
@@ -1,27 +1,31 @@
 // Give the service worker access to Firebase Messaging.
 // Note that you can only use Firebase Messaging here, other Firebase libraries
 // are not available in the service worker.
-importScripts("https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js");
-importScripts("https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js");
-importScripts("sw-firebase-config.js");
+importScripts(
+  'https://www.gstatic.com/firebasejs/4.8.1/firebase-app.js'
+);
+importScripts(
+  'https://www.gstatic.com/firebasejs/4.8.1/firebase-messaging.js'
+);
+importScripts('sw-firebase-config.js');
 
 // message listener
-self.addEventListener("message", event => {
-  console.log("Notification SW received message:", event.data);
+self.addEventListener('message', (event) => {
+  console.log('Notification SW received message:', event.data);
 });
 
-self.addEventListener("notificationclick", function(event) {
-  const rootUrl = new URL("/", location).href;
+self.addEventListener('notificationclick', function(event) {
+  const rootUrl = new URL('/', location).href;
   event.notification.close();
   // Enumerate windows, and call window.focus(), or open a new one.
   event.waitUntil(
-    clients.matchAll().then(matchedClients => {
+    clients.matchAll().then((matchedClients) => {
       for (let client of matchedClients) {
         if (client.url === rootUrl) {
           return client.focus();
         }
       }
-      return clients.openWindow("/");
+      return clients.openWindow('/user');
     })
   );
 });
@@ -30,13 +34,13 @@ self.addEventListener("notificationclick", function(event) {
 firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
-messaging.setBackgroundMessageHandler(payload => {
-  console.log("Received background message ", payload);
+messaging.setBackgroundMessageHandler((payload) => {
+  console.log('Received background message ', payload);
   // here you can override some options describing what's in the message;
   // however, the actual content will come from the Webtask
   const notificationTitle = payload.data.title;
   const notificationOptions = {
-    icon: "/assets/icons/logo-128x128.png"
+    icon: '/assets/icons/PS_logo_128x128.png',
   };
   return self.registration.showNotification(
     notificationTitle,
